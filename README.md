2c328c403793815d67d35aa8d4d6d66f97db4786
songyingxu <yxsong@ecut.edu.cn>
2021-04-27 11:44:05 +0800

    Change the code for submitting to cageo

--- README.md
+++ README.md
@@ -1,5 +1,1 @@
-# imblearn in LSM 
-
-reference：https://imbalanced-learn.org/stable/auto_examples/applications/plot_over_sampling_benchmark_lfw.html#sphx-glr-auto-examples-applications-plot-over-sampling-benchmark-lfw-py
-
-https://imbalanced-learn.org/stable/auto_examples/ensemble/plot_comparison_ensemble_classifier.html#sphx-glr-auto-examples-ensemble-plot-comparison-ensemble-classifier-py
+# Landslide Susceptibility Prediction Models
--- main.py
+++ main.py
@@ -4,19 +4,23 @@ import pickle
 import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
-from imblearn.ensemble import BalancedRandomForestClassifier
+from imblearn.datasets import fetch_datasets
+from imblearn.ensemble import (BalancedBaggingClassifier,
+                               BalancedRandomForestClassifier,
+                               EasyEnsembleClassifier, RUSBoostClassifier)
 from imblearn.metrics import geometric_mean_score
 from imblearn.pipeline import make_pipeline
 from lightgbm import LGBMClassifier
 from sklearn import datasets, preprocessing
 from sklearn.decomposition import PCA
-from sklearn.ensemble import RandomForestClassifier
+from sklearn.ensemble import (AdaBoostClassifier, BaggingClassifier,
+                              RandomForestClassifier)
 from sklearn.linear_model import LogisticRegressionCV
-
-from sklearn.metrics import (balanced_accuracy_score, classification_report,
-                             confusion_matrix, recall_score, roc_auc_score,
-                             roc_curve, auc,accuracy_score)
+from sklearn.metrics import (accuracy_score, auc, balanced_accuracy_score,
+                             classification_report, confusion_matrix,
+                             recall_score, roc_auc_score, roc_curve)
 from sklearn.model_selection import train_test_split
+from sklearn.tree import DecisionTreeClassifier
 
 
 def plot_confusion_matrix(cm, classes, ax,
@@ -47,24 +51,16 @@ def plot_confusion_matrix(cm, classes, ax,
     ax.set_ylabel('True label')
     ax.set_xlabel('Predicted label')
 
-def data_raw(data):
-    target = 'value'
-    IDCol = 'ID'
-    GeoID = data[IDCol]
-    print(data[target].value_counts())
-#     x_columns = [x for x in data.columns if x not in [target,IDCol,'GRID_CODE']]
-    x_columns = ['Elevation', 'Slope', 'Aspect', 'TRI', 'Curvature', 'Lithology', 'River', 'NDVI', 'NDWI', 'Rainfall', 'Earthquake', 'Land_use']
+################
+#Data Prepare
+################
+train = pd.read_csv('data/wanzhou_island.csv')
+train_labels = train['value']
+x_columns = ['Elevation', 'Slope', 'Aspect', 'TRI', 'Curvature', 'Lithology', 'River', 'NDVI', 'NDWI', 'Rainfall', 'Earthquake', 'Land_use']
+GeoID = train['ID']
 
-    X = data[x_columns]
-    y = data[target]
-    return X, y, GeoID
-###############################################################################
-# Load an imbalanced dataset
-###############################################################################
-
-data = pd.read_csv('./data/wanzhou_island.csv')
-X, y, GeoID = data_raw(data)
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.4,stratify=y, random_state=0)
+X, y = train[x_columns], train_labels
+X_train, X_test, y_train, y_test = train_test_split(X, y, stratify=y, random_state=0)
 
 # Instanciate a PCA object
 pca = PCA(n_components='mle')
@@ -72,22 +68,20 @@ pca = PCA(n_components='mle')
 # Instanciate a StandardScaler object
 stdscaler = preprocessing.StandardScaler()
 
-###############################################################################
 # Classification using LogisticRegressionCV classifier with and without sampling
-###############################################################################
 
 lr = LogisticRegressionCV(cv=5, random_state=0)
-blr = LogisticRegressionCV(cv=5, random_state=0, class_weight='balanced')
+wlr = LogisticRegressionCV(cv=5, random_state=0, class_weight={0:1, 1:19})
 
 # Add one transformers and a sampler in the pipeline object
 pipeline_lr = make_pipeline(stdscaler, pca, lr)
-pipeline_blr = make_pipeline(stdscaler, pca, blr)
+pipeline_wlr = make_pipeline(stdscaler, pca, wlr)
 
 pipeline_lr.fit(X_train, y_train)
-pipeline_blr.fit(X_train, y_train)
+pipeline_wlr.fit(X_train, y_train)
 
 y_pred_lr = pipeline_lr.predict(X_test)
-y_pred_blr = pipeline_blr.predict(X_test)
+y_pred_wlr = pipeline_wlr.predict(X_test)
 
 print('Logistic Regression classifier performance:')
 print('Balanced accuracy: {:.3f} - Geometric mean {:.3f} - Recall {:.3f} - Accuracy {:.3f}'
@@ -100,32 +94,29 @@ plot_confusion_matrix(cm_rf, classes=[0,1], ax=ax[0],
 
 print('Balanced Logistic Regression classifier performance:')
 print('Balanced accuracy: {:.3f} - Geometric mean {:.3f}- Recall {:.3f} - Accuracy {:.3f}'
-      .format(balanced_accuracy_score(y_test, y_pred_blr),
-              geometric_mean_score(y_test, y_pred_blr),recall_score(y_test, y_pred_blr),accuracy_score(y_test, y_pred_blr)))
-cm_brf = confusion_matrix(y_test, y_pred_blr)
-plot_confusion_matrix(cm_brf, classes=[0,1], ax=ax[1],
+      .format(balanced_accuracy_score(y_test, y_pred_wlr),
+              geometric_mean_score(y_test, y_pred_wlr),recall_score(y_test, y_pred_wlr),accuracy_score(y_test, y_pred_wlr)))
+cm_wrf = confusion_matrix(y_test, y_pred_wlr)
+plot_confusion_matrix(cm_wrf, classes=[0,1], ax=ax[1],
                       title='Weighted Logistic Regression')
 plt.show()
 
 ###############################################################################
 # Classification using random forest classifier with and without sampling
 ###############################################################################
-# Random forest is another popular ensemble method and it is usually
-# outperforming bagging. Here, we used a vanilla random forest and its balanced
-# counterpart in which each bootstrap sample is balanced.
 
-rf = RandomForestClassifier(random_state=0, n_jobs=-1)
-brf = BalancedRandomForestClassifier(random_state=0, n_jobs=-1)
+rf = RandomForestClassifier(random_state=0)
+wrf = BalancedRandomForestClassifier(random_state=0)
 
 # Add one transformers and a sampler in the pipeline object
 pipeline_rf = make_pipeline(stdscaler, pca, rf)
-pipeline_brf = make_pipeline(stdscaler, pca, brf)
+pipeline_wrf = make_pipeline(stdscaler, pca, wrf)
 
 pipeline_rf.fit(X_train, y_train)
-pipeline_brf.fit(X_train, y_train)
+pipeline_wrf.fit(X_train, y_train)
 
 y_pred_rf = pipeline_rf.predict(X_test)
-y_pred_brf = pipeline_brf.predict(X_test)
+y_pred_wrf = pipeline_wrf.predict(X_test)
 
 # Similarly to the previous experiment, the balanced classifier outperform the
 # classifier which learn from imbalanced bootstrap samples. In addition, random
@@ -138,15 +129,14 @@ print('Balanced accuracy: {:.3f} - Geometric mean {:.3f} - Recall {:.3f} - Accur
 cm_rf = confusion_matrix(y_test, y_pred_rf)
 fig, ax = plt.subplots(ncols=2)
 plot_confusion_matrix(cm_rf, classes=[0,1], ax=ax[0],
-                      title='Random Forest')
+                      title='RF')
 
 print('Balanced Random Forest classifier performance:')
 print('Balanced accuracy: {:.3f} - Geometric mean {:.3f}- Recall {:.3f} - Accuracy {:.3f}'
-      .format(balanced_accuracy_score(y_test, y_pred_brf),
-              geometric_mean_score(y_test, y_pred_brf),recall_score(y_test, y_pred_brf),accuracy_score(y_test, y_pred_brf)))
-cm_brf = confusion_matrix(y_test, y_pred_brf)
-plot_confusion_matrix(cm_brf, classes=[0,1], ax=ax[1],
-                      title='Weighted Random Forest')
+      .format(balanced_accuracy_score(y_test, y_pred_wrf),
+              geometric_mean_score(y_test, y_pred_wrf),recall_score(y_test, y_pred_wrf),accuracy_score(y_test, y_pred_wrf)))
+cm_wrf = confusion_matrix(y_test, y_pred_wrf)
+plot_confusion_matrix(cm_wrf, classes=[0,1], ax=ax[1], title='WRF')
 
 plt.show()
 ###############################################################################
@@ -154,17 +144,17 @@ plt.show()
 ###############################################################################
 
 lgb = LGBMClassifier(random_state=0, n_jobs=-1)
-blgb = LGBMClassifier(random_state=0, n_jobs=-1, class_weight='balanced')
+wlgb = LGBMClassifier(random_state=0, n_jobs=-1, class_weight={0:1, 1:19})
 
 # Add one transformers and a sampler in the pipeline object
 pipeline_lgb = make_pipeline(stdscaler, pca, lgb)
-pipeline_blgb = make_pipeline(stdscaler, pca, blgb)
+pipeline_wlgb = make_pipeline(stdscaler, pca, wlgb)
 
 pipeline_lgb.fit(X_train, y_train)
-pipeline_blgb.fit(X_train, y_train)
+pipeline_wlgb.fit(X_train, y_train)
 
 y_pred_lgb = pipeline_lgb.predict(X_test)
-y_pred_blgb = pipeline_blgb.predict(X_test)
+y_pred_wlgb = pipeline_wlgb.predict(X_test)
 
 print('LightGBM classifier performance:')
 print('Balanced accuracy: {:.3f} - Geometric mean {:.3f} - Recall {:.3f} - Accuracy {:.3f}'
@@ -178,57 +168,57 @@ plot_confusion_matrix(cm_lgb, classes=[0,1], ax=ax[0],
 
 print('Weighted LightGBM classifier performance:')
 print('Balanced accuracy: {:.3f} - Geometric mean {:.3f} - Recall {:.3f} - Accuracy {:.3f}'
-      .format(balanced_accuracy_score(y_test, y_pred_blgb),
-              geometric_mean_score(y_test, y_pred_blgb), recall_score(y_test, y_pred_blgb), accuracy_score(y_test,y_pred_blgb)))
+      .format(balanced_accuracy_score(y_test, y_pred_wlgb),
+              geometric_mean_score(y_test, y_pred_wlgb), recall_score(y_test, y_pred_wlgb), accuracy_score(y_test,y_pred_wlgb)))
 
-cm_blgb = confusion_matrix(y_test, y_pred_blgb)
-plot_confusion_matrix(cm_blgb, classes=[0,1], ax=ax[1],
-                      title='Weighted LightGBM')
+cm_wlgb = confusion_matrix(y_test, y_pred_wlgb)
+plot_confusion_matrix(cm_wlgb, classes=[0,1], ax=ax[1],
+                      title='WLightGBM')
 plt.show()
 ###############################################################################
 # Plot ROC Curve
 ###############################################################################
 
-# The lr model by itself
+# The lr model 
 y_pred_lr_p = pipeline_lr.predict_proba(X_test)[:, 1]
 fpr_lr, tpr_lr, _ = roc_curve(y_test, y_pred_lr_p)
 auc_lr = roc_auc_score(y_test,y_pred_lr_p)
 
-# The weighted lr model by itself
-y_pred_blr_p = pipeline_blr.predict_proba(X_test)[:, 1]
-fpr_blr, tpr_blr, _ = roc_curve(y_test, y_pred_blr_p)
-auc_blr = roc_auc_score(y_test,y_pred_blr_p)
+# The weighted lr model 
+y_pred_wlr_p = pipeline_wlr.predict_proba(X_test)[:, 1]
+fpr_wlr, tpr_wlr, _ = roc_curve(y_test, y_pred_wlr_p)
+auc_wlr = roc_auc_score(y_test,y_pred_wlr_p)
 
-# The gbdt model by itself
+# The lightgmb model 
 y_pred_lgb_p = pipeline_lgb.predict_proba(X_test)[:, 1]
 fpr_lgb, tpr_lgb, _ = roc_curve(y_test, y_pred_lgb_p)
 auc_lgb = roc_auc_score(y_test,y_pred_lgb_p)
 
-# The weighted gbdt model by itself
-y_pred_blgb_p = pipeline_blgb.predict_proba(X_test)[:, 1]
-fpr_blgb, tpr_blgb, _ = roc_curve(y_test, y_pred_blgb_p)
-auc_blgb = roc_auc_score(y_test,y_pred_blgb_p)
+# The weighted lightgmb model 
+y_pred_wlgb_p = pipeline_wlgb.predict_proba(X_test)[:, 1]
+fpr_wlgb, tpr_wlgb, _ = roc_curve(y_test, y_pred_wlgb_p)
+auc_wlgb = roc_auc_score(y_test,y_pred_wlgb_p)
 
-# The rf model by itself
+# The rf model 
 y_pred_rf_p = pipeline_rf.predict_proba(X_test)[:, 1]
 fpr_rf, tpr_rf, _ = roc_curve(y_test, y_pred_rf_p)
 auc_rf = roc_auc_score(y_test,y_pred_rf_p)
 
-# The weighted rf model by itself
-y_pred_brf_p = pipeline_brf.predict_proba(X_test)[:, 1]
-fpr_brf, tpr_brf, _ = roc_curve(y_test, y_pred_brf_p)
-auc_brf = roc_auc_score(y_test,y_pred_brf_p)
+# The weighted rf model 
+y_pred_wrf_p = pipeline_wrf.predict_proba(X_test)[:, 1]
+fpr_wrf, tpr_wrf, _ = roc_curve(y_test, y_pred_wrf_p)
+auc_wrf = roc_auc_score(y_test,y_pred_wrf_p)
 
 plt.figure(1)
 plt.plot([0, 1], [0, 1], 'k--')
 plt.plot(fpr_lr, tpr_lr, label='LR (AUC=%0.3f)' % (auc_lr), lw=2)
-plt.plot(fpr_blr, tpr_blr, label='Weighted LR (AUC=%0.3f)' % (auc_blr), lw=2)
+plt.plot(fpr_wlr, tpr_wlr, label='WLR (AUC=%0.3f)' % (auc_wlr), lw=2)
 
 plt.plot(fpr_lgb, tpr_lgb, label='LightGBM (AUC=%0.3f)' % (auc_lgb), lw=2)
-plt.plot(fpr_blgb, tpr_blgb, label='Weighted LightGBM(AUC=%0.3f)' % (auc_blgb), lw=2)
+plt.plot(fpr_wlgb, tpr_wlgb, label='WLightGBM(AUC=%0.3f)' % (auc_wlgb), lw=2)
 
 plt.plot(fpr_rf, tpr_rf, label='RF (AUC=%0.3f)' % (auc_rf), lw=2)
-plt.plot(fpr_brf, tpr_brf, label='WRF(AUC=%0.3f)' % (auc_brf), lw=2)
+plt.plot(fpr_wrf, tpr_wrf, label='WRF(AUC=%0.3f)' % (auc_wrf), lw=2)
 
 plt.xlabel('False positive rate')
 plt.ylabel('True positive rate')
@@ -251,12 +241,12 @@ def save_results(GeoID, y_pred, y_predprob, result_file):
 
 y_pred_gdbt = pipeline_lgb.predict(X)
 y_pred_proba_gdbt = pipeline_lgb.predict_proba(X)[:,1]
-result_file_gdbt = './data/gbdt.txt'
+result_file_gdbt = './data/lightgbm.txt'
 save_results(GeoID, y_pred_gdbt, y_pred_proba_gdbt, result_file_gdbt)
 
-y_pred_gdbt_weighted = pipeline_blgb.predict(X)
-y_pred_proba_gdbt_weighted = pipeline_blgb.predict_proba(X)[:,1]
-result_file_gdbt_weighted = './data/gdbt_weighted.txt'
+y_pred_gdbt_weighted = pipeline_wlgb.predict(X)
+y_pred_proba_gdbt_weighted = pipeline_wlgb.predict_proba(X)[:,1]
+result_file_gdbt_weighted = './data/wlightgbm.txt'
 save_results(GeoID, y_pred_gdbt_weighted, y_pred_proba_gdbt_weighted, result_file_gdbt_weighted)
 
 y_pred_lr = pipeline_lr.predict(X)
@@ -264,19 +254,19 @@ y_pred_proba_lr = pipeline_lr.predict_proba(X)[:,1]
 result_file_lr = './data/lr.txt'
 save_results(GeoID, y_pred_lr, y_pred_proba_lr, result_file_lr)
 
-y_pred_blr = pipeline_blr.predict(X)
-y_pred_proba_blr = pipeline_blr.predict_proba(X)[:,1]
-result_file_blr = './data/blr.txt'
-save_results(GeoID, y_pred_blr, y_pred_proba_blr, result_file_blr)
+y_pred_wlr = pipeline_wlr.predict(X)
+y_pred_proba_wlr = pipeline_wlr.predict_proba(X)[:,1]
+result_file_wlr = './data/wlr.txt'
+save_results(GeoID, y_pred_wlr, y_pred_proba_wlr, result_file_wlr)
 
 y_pred_rf = pipeline_rf.predict(X)
 y_pred_proba_rf = pipeline_rf.predict_proba(X)[:,1]
 result_file_rf = './data/rf.txt'
 save_results(GeoID, y_pred_rf, y_pred_proba_rf, result_file_rf)
 
-y_pred_brf = pipeline_brf.predict(X)
-y_pred_proba_brf = pipeline_brf.predict_proba(X)[:,1]
-result_file_brf = './data/brf.txt'
-save_results(GeoID, y_pred_brf, y_pred_proba_brf, result_file_brf)
+y_pred_wrf = pipeline_wrf.predict(X)
+y_pred_proba_wrf = pipeline_wrf.predict_proba(X)[:,1]
+result_file_wrf = './data/wrf.txt'
+save_results(GeoID, y_pred_wrf, y_pred_proba_wrf, result_file_wrf)
 
-print('Done')
+print('Done!')
